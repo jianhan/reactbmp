03d74b11074e509ca5128087da5e52fa3911e51a
Jian Han <jianhan@mail.com>
2018-06-07 20:53:29 +1000

    [DEV] firebase utils ready

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,9 +2,11 @@
  *  have seen previously on your Firebase dashboard. In addition, Firebase itself will be instantiated in this file.
  */
 
-import firebase from 'firebase/app'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
-const config = {
+const prodConfig = {
     apiKey: "AIzaSyCTPbL9CboXx3bC_QqG_XxXBF0eatwfBeY",
     authDomain: "reactfire-198405.firebaseapp.com",
     databaseURL: "https://reactfire-198405.firebaseio.com",
@@ -13,6 +15,27 @@ const config = {
     messagingSenderId: "538077058493"
 };
 
+const devConfig = {
+    apiKey: "AIzaSyCTPbL9CboXx3bC_QqG_XxXBF0eatwfBeY",
+    authDomain: "reactfire-198405.firebaseapp.com",
+    databaseURL: "https://reactfire-198405.firebaseio.com",
+    projectId: "reactfire-198405",
+    storageBucket: "reactfire-198405.appspot.com",
+    messagingSenderId: "538077058493"
+};
+
+const config = process.env.NODE_ENV === 'production'
+    ? prodConfig
+    : devConfig;
+
 if (!firebase.apps.length) {
-    firebase.initializeApp(config)
+    firebase.initializeApp(config);
 }
+
+const db = firebase.database();
+const auth = firebase.auth();
+
+export {
+    db,
+    auth,
+};
